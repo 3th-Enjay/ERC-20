db9f63b56efbd4ff2122f91df6b1f4201bb501c4
3th-Enjay <enjayjerey@gmail.com>
2025-03-23 23:55:37 +0100

    Update network configuration to use INFURA_API_URL instead of ALCHEMY_API_URL

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.19",
   networks: {
     sepolia: {
-      url: process.env.ALCHEMY_API_URL,
+      url: process.env.INFURA_API_URL,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
